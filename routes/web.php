74b6b4dd63a5b4c1048ea270597f26e8552ad35c
Najibu Nsubuga <ns.najibu@gmail.com>
2022-01-06 12:14:45 +0000

    added search

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Models\Category;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +17,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
-Route::get('/post', function () {
-    return view('post');
+Route::get('/', [PostController::class, 'index'])->name('home');
+
+
+Route::get('/posts/{post:slug}', [PostController::class, 'show']);
+
+Route::get('/categories/{category:slug}', function (Category $category) {
+    return view('posts', [
+        'posts' => $category->posts,
+        'currentCategory' => $category,
+        'categories' => Category::all(),
+    ]);
+})->name('category');
+
+Route::get('/authors/{author:username}', function (User $author) {
+    return view('posts', [
+        'posts' => $author->posts,
+        'categories' => Category::all(),
+    ]);
 });
