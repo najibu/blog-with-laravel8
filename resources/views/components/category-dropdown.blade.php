d42606747d965729529c99c6bfd21a083132e14d
Najibu Nsubuga <ns.najibu@gmail.com>
2022-01-06 13:57:32 +0000

    paginate

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -7,13 +7,15 @@
         </button>
     </x-slot>
 
-    <x-dropdown-item href="/" :active="request()->routeIs('home')">
+    <x-dropdown-item
+        href="/?{{ http_build_query(request()->except('category', 'page')) }}"
+        :active="request()->routeIs('home')">
         All
     </x-dropdown-item>
 
     @foreach ($categories as $category)
         <x-dropdown-item
-            href="/?category={{ $category->slug }}&{{ http_build_query(request()->except('category')) }}"
+            href="/?category={{ $category->slug }}&{{ http_build_query(request()->except('category', 'page')) }}"
             :active='request()->is("categories/{$category->slug}")'>
             {{ ucwords($category->name) }}
         </x-dropdown-item>
