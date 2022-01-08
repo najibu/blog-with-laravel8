2ab0969aa0541b94fff9b5f5a156c532da3842b3
Najibu Nsubuga <ns.najibu@gmail.com>
2022-01-08 11:56:34 +0000

    breeze install

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -7,17 +7,19 @@
         </button>
     </x-slot>
 
-    <x-dropdown-item
-        href="/?{{ http_build_query(request()->except('category', 'page')) }}"
-        :active="request()->routeIs('home')">
-        All
-    </x-dropdown-item>
-
-    @foreach ($categories as $category)
+    <x-slot name="content">
         <x-dropdown-item
-            href="/?category={{ $category->slug }}&{{ http_build_query(request()->except('category', 'page')) }}"
-            :active='request()->is("categories/{$category->slug}")'>
-            {{ ucwords($category->name) }}
+            href="/?{{ http_build_query(request()->except('category', 'page')) }}"
+            :active="request()->routeIs('home')">
+            All
         </x-dropdown-item>
-    @endforeach
+
+        @foreach ($categories as $category)
+            <x-dropdown-item
+                href="/?category={{ $category->slug }}&{{ http_build_query(request()->except('category', 'page')) }}"
+                :active='request()->is("categories/{$category->slug}")'>
+                {{ ucwords($category->name) }}
+            </x-dropdown-item>
+        @endforeach
+    </x-slot>
 </x-dropdown>
--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,14 @@ use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('/posts/{post:slug}', [PostController::class, 'show']);
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
